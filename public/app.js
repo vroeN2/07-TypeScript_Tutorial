393d677f546c40ea4fae8fd14ceac4091c1cfe2c
Maciej Zachariasz <47660926+vroeN2@users.noreply.github.com>
2021-11-25 09:42:36 +0100

    [L-14] modules
    split TS file into two separate ones, export and import required elements between them, change tsconfig to make that work

--- public/app.js
+++ public/app.js
@@ -1,27 +1,6 @@
-"use strict";
-// classes
-// keyword 'public' is a default value - there is no need to write that, but in case we want, we can :)
-// if we add a keyword 'private' before class property declaration, it will become unaccessible from outside of that class. 
-// as expected, 'readonly' keyword makes a property accessible for read only. it can't be changed outside of the class.
-class Invoice {
-    // readonly client: string;
-    // private details: string;
-    // public amount: number;
-    // we can also skip above, and just define them in the constructor BUT each property has to be preceded by one of the keywords!
-    constructor(/*c: string, d: string, a: number */ client, details, amount) {
-        this.client = client;
-        this.details = details;
-        this.amount = amount;
-        // this.client = c;
-        // this.details = d;
-        // this.amount = a;
-    }
-    format() {
-        return `${this.client} owes €${this.amount} for ${this.details}`;
-    }
-}
-const invOne = new Invoice('mario', 'work on mario website', 250);
-const invTwo = new Invoice('luigi', 'work on luigi website', 300);
+import { Invoice } from './classes/Invoice.js';
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
 let invoices = [];
 invoices.push(invOne);
 invoices.push(invTwo);
--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,7 @@
-// classes
-// keyword 'public' is a default value - there is no need to write that, but in case we want, we can :)
-// if we add a keyword 'private' before class property declaration, it will become unaccessible from outside of that class. 
-// as expected, 'readonly' keyword makes a property accessible for read only. it can't be changed outside of the class.
+import { Invoice } from './classes/Invoice.js'
 
-class Invoice {
-    // readonly client: string;
-    // private details: string;
-    // public amount: number;
-
-    // we can also skip above, and just define them in the constructor BUT each property has to be preceded by one of the keywords!
-    constructor(/*c: string, d: string, a: number */
-            readonly client: string,
-            private details: string,
-            public amount: number,
-        ) {
-        // this.client = c;
-        // this.details = d;
-        // this.amount = a;
-    }
-
-    format() {
-        return `${this.client} owes €${this.amount} for ${this.details}`;
-    }
-}
-
-
-const invOne = new Invoice('mario', 'work on mario website', 250);
-const invTwo = new Invoice('luigi', 'work on luigi website', 300);
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
 
 let invoices: Invoice[] = [];
 invoices.push(invOne);
@@ -34,7 +9,7 @@ invoices.push(invTwo);
 
 invoices.forEach(inv => {
     console.log(inv.client, inv.amount, inv.format());
-})
+});
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
