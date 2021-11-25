636a041a7a4c85a11f86d898fb99cc077d6bb0b1
Maciej Zachariasz <47660926+vroeN2@users.noreply.github.com>
2021-11-25 11:27:00 +0100

    [L-17] rendering an html template
    create list item template and include it in the even listener function

--- public/app.js
+++ public/app.js
@@ -1,28 +1,14 @@
 import { Invoice } from './classes/Invoice.js';
+import { ListTemplate } from './classes/ListTemplate.js';
 import { Payment } from './classes/Payment.js';
-// // lines below structures our code a bit more, and makes things a little bit more secure in the future
-// let docOne: HasFormatter;
-// let docTwo: HasFormatter;
-// docOne = new Invoice('yoshi', 'web work', 250);
-// docTwo = new Payment('mario', 'plumbing work', 200);
-// let docs: HasFormatter[] = []; // we just restricted that this array will contain only elements that follows 'HasFormatter' rules. They can come from any number of different classes, as long as they follow 'HasFormatter' rule set. This means, we are sure that any element in this array has a format function that returns a string.
-// docs.push(docOne);
-// docs.push(docTwo);
-// console.log(docs);
-// const invOne = new Invoice('mario', 'work on the mario website', 250);
-// const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
-// let invoices: Invoice[] = [];
-// invoices.push(invOne)
-// invoices.push(invTwo);
-// invoices.forEach(inv => {
-//   console.log(inv.client, /*inv.details,*/ inv.amount, inv.format());
-// })
 const form = document.querySelector('.new-item-form');
 // inputs
 const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     let doc;
@@ -32,5 +18,5 @@ form.addEventListener('submit', (e) => {
     else {
         doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
     }
-    console.log(doc);
+    list.render(doc, type.value, 'end');
 });
--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,7 @@
 import { Invoice } from './classes/Invoice.js';
+import { ListTemplate } from './classes/ListTemplate.js';
 import { Payment } from './classes/Payment.js';
 import { HasFormatter } from './interfaces/HasFormatter.js';
-
-// // lines below structures our code a bit more, and makes things a little bit more secure in the future
-// let docOne: HasFormatter;
-// let docTwo: HasFormatter;
-
-// docOne = new Invoice('yoshi', 'web work', 250);
-// docTwo = new Payment('mario', 'plumbing work', 200);
-
-// let docs: HasFormatter[] = []; // we just restricted that this array will contain only elements that follows 'HasFormatter' rules. They can come from any number of different classes, as long as they follow 'HasFormatter' rule set. This means, we are sure that any element in this array has a format function that returns a string.
-// docs.push(docOne);
-// docs.push(docTwo);
-
-// console.log(docs);
-
-
-// const invOne = new Invoice('mario', 'work on the mario website', 250);
-// const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
-
-// let invoices: Invoice[] = [];
-// invoices.push(invOne)
-// invoices.push(invTwo);
-
-// invoices.forEach(inv => {
-//   console.log(inv.client, /*inv.details,*/ inv.amount, inv.format());
-// })
-
-
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
@@ -36,6 +10,9 @@ const type = document.querySelector('#type') as HTMLInputElement;
 const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
+
+const ul = document.querySelector('ul')!;
+const list = new ListTemplate(ul);
 
 form.addEventListener('submit', (e: Event) => {
   e.preventDefault();
@@ -47,5 +24,5 @@ form.addEventListener('submit', (e: Event) => {
     doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
   }
 
-  console.log(doc);
+  list.render(doc, type.value, 'end');
 });
