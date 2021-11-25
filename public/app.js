d285253f4c26d21fb3689b4ed09c7dc45dcfb7cb
Maciej Zachariasz <47660926+vroeN2@users.noreply.github.com>
2021-11-25 09:16:57 +0100

    [L-12] classes
    add a new class, create temporary elements for tutorial purposes, learn how do they work

--- public/app.js
+++ public/app.js
@@ -1,14 +1,21 @@
 "use strict";
-// const anchor = document.querySelector('a');
-// console.log(anchor); // it works, but what if we want to acces the 'href' property of that anchor tag? It will cause an error - TS doesn't know if there is a 'href' property, thus in case it doesn't exist, it throws an error. we can work around that in two ways:
-// if (anchor) {
-//     console.log(anchor.href)
-// }
-// const anchor2 = document.querySelector('a')!; 
-// console.log(anchor.href);
-// exclamation mark will tell the browser that we are sure that there is a 'href' property, so it can worry not ;)
-// another thing is that TS automatically contains special types for every DOM elements, so if we declare one, TS knows it's properties and methods, and it make them available for us in an element tooltip.
-// if we grab an element by it's class name, TS will treat this one as an Element, but without specification (like 'form' or 'anchor tag') - it is because class name can be applied to any different element in the HTML page, so it doesn't know that it is a form, it just knows it is going to be some kind of elements. In that case, we might want to use something known as typecasting to say what this is going to be, what type of element. 
+// classes
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owes €${this.amount} for ${this.details}`;
+    }
+}
+const invOne = new Invoice('mario', 'work on mario website', 250);
+const invTwo = new Invoice('luigi', 'work on luigi website', 300);
+let invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+console.log(invoices);
 const form = document.querySelector('.new-item-form');
 // inputs
 const type = document.querySelector('#type');
@@ -17,7 +24,5 @@ const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log(type.value, toFrom.value, details.value, amount.valueAsNumber
-    // valueAsNumber will make vale a number - shocker ;p but in TS it might be important!
-    );
+    console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,29 @@
-// const anchor = document.querySelector('a');
+// classes
+class Invoice {
+    client: string;
+    details: string;
+    amount: number;
 
-// console.log(anchor); // it works, but what if we want to acces the 'href' property of that anchor tag? It will cause an error - TS doesn't know if there is a 'href' property, thus in case it doesn't exist, it throws an error. we can work around that in two ways:
+    constructor(c: string, d: string, a: number) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
 
-// if (anchor) {
-//     console.log(anchor.href)
-// }
+    format() {
+        return `${this.client} owes €${this.amount} for ${this.details}`;
+    }
+}
 
-// const anchor2 = document.querySelector('a')!; 
-// console.log(anchor.href);
-// exclamation mark will tell the browser that we are sure that there is a 'href' property, so it can worry not ;)
+const invOne = new Invoice('mario', 'work on mario website', 250);
+const invTwo = new Invoice('luigi', 'work on luigi website', 300);
 
-// another thing is that TS automatically contains special types for every DOM elements, so if we declare one, TS knows it's properties and methods, and it make them available for us in an element tooltip.
+let invoices: Invoice[] = [];
+invoices.push(invOne);
+invoices.push(invTwo);
 
+console.log(invoices);
 
-// if we grab an element by it's class name, TS will treat this one as an Element, but without specification (like 'form' or 'anchor tag') - it is because class name can be applied to any different element in the HTML page, so it doesn't know that it is a form, it just knows it is going to be some kind of elements. In that case, we might want to use something known as typecasting to say what this is going to be, what type of element. 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
 // inputs
@@ -24,11 +34,11 @@ const amount = document.querySelector('#amount') as HTMLInputElement;
 
 form.addEventListener('submit', (e: Event) => {
     e.preventDefault();
+
     console.log(
         type.value,
         toFrom.value,
         details.value,
         amount.valueAsNumber
-        // valueAsNumber will make vale a number - shocker ;p but in TS it might be important!
     )
 })
