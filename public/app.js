f2af65eec42ee1b2a9a92dd6c4c37e8274c4b067
Maciej Zachariasz <47660926+vroeN2@users.noreply.github.com>
2021-11-25 12:00:22 +0100

    [L-18] generics
    what are they and how to create and use them

--- public/app.js
+++ public/app.js
@@ -20,3 +20,39 @@ form.addEventListener('submit', (e) => {
     }
     list.render(doc, type.value, 'end');
 });
+// GENERICS
+// // if we want to create function, that takes an object, and adds a random ID to it, we can start like this:
+// const addUID = (pbj: object) => {
+// // but then, we won't be able to access it's properties, like
+// console.log(docOne.name) // expected output: error
+// // it is because we can use any object here, so it can or cannot have a property called 'name'. To work around that, we can do it like this:
+// const addUID = <T>(obj: T) {
+// // and it will know all properties of the object. The new problem that appears here is that we can now use not only objects, but also any other type, for example
+// let docOne = addUID('hello);
+// // and we don't want that to be possible. We can then be more specific with T:
+// const addUID = <T extends object>(obj: T) {
+// // or even more specific, so now it has to be an object, but with 'name' property that has a string value:
+const addUID = (obj) => {
+    let uid = Math.floor(Math.random() * 100);
+    return Object.assign(Object.assign({}, obj), { uid });
+};
+let docOne = addUID({ name: 'yoshi', age: 40 });
+console.log(docOne);
+// with that, we can be more flexible:
+const docTwo = {
+    uid: 1,
+    resourceName: 'person',
+    data: 'string'
+};
+// or 
+const docThree = {
+    uid: 2,
+    resourceName: 'person 2',
+    data: { name: 'shaun' }
+};
+const docFour = {
+    uid: 3,
+    resourceName: 'shoppingList',
+    data: ['bread', 'milk', 'butter']
+};
+console.log(docTwo, docThree, docFour);
--- src/app.ts
+++ src/app.ts
@@ -26,3 +26,59 @@ form.addEventListener('submit', (e: Event) => {
 
   list.render(doc, type.value, 'end');
 });
+
+
+// GENERICS
+
+// // if we want to create function, that takes an object, and adds a random ID to it, we can start like this:
+// const addUID = (pbj: object) => {
+// // but then, we won't be able to access it's properties, like
+// console.log(docOne.name) // expected output: error
+// // it is because we can use any object here, so it can or cannot have a property called 'name'. To work around that, we can do it like this:
+// const addUID = <T>(obj: T) {
+// // and it will know all properties of the object. The new problem that appears here is that we can now use not only objects, but also any other type, for example
+// let docOne = addUID('hello);
+// // and we don't want that to be possible. We can then be more specific with T:
+// const addUID = <T extends object>(obj: T) {
+// // or even more specific, so now it has to be an object, but with 'name' property that has a string value:
+const addUID = <T extends {name: string}>(obj: T) => {
+  let uid = Math.floor(Math.random() * 100);
+  return { ...obj, uid};
+}
+
+let docOne = addUID({name: 'yoshi', age: 40});
+console.log(docOne);
+
+
+// // with interfaces
+// interface Resource {
+//   uid: number;
+//   resourceName: string,
+//   data: '???';
+// }
+// the whole point is that we don't know what type data will be, so to avoid errors, we can also use generic here:
+interface Resource<T> {
+  uid: number;
+  resourceName: string,
+  data: T;
+}
+// with that, we can be more flexible:
+const docTwo: Resource<string> = {
+  uid: 1,
+  resourceName: 'person',
+  data: 'string'
+}
+// or 
+const docThree: Resource<object> = {
+  uid: 2,
+  resourceName: 'person 2',
+  data: { name: 'shaun' }
+}
+
+const docFour: Resource<string[]> = {
+  uid: 3,
+  resourceName: 'shoppingList',
+  data: ['bread', 'milk', 'butter']
+}
+
+console.log(docTwo, docThree, docFour);
