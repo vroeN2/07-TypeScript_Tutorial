8311dd3ba8c5e312f96d2db16258e661fcd4dffb
Maciej Zachariasz <47660926+vroeN2@users.noreply.github.com>
2021-11-25 12:28:40 +0100

    [L-20] tuples
    when they are useful, how to define one and how to use it after

--- src/app.ts
+++ src/app.ts
@@ -17,40 +17,39 @@ const list = new ListTemplate(ul);
 form.addEventListener('submit', (e: Event) => {
   e.preventDefault();
 
+  // because we use tuples here, TS now knows that values contains a string, another string and a number
+  let values: [string, string, number];
+  // so we can define it like this
+  values = [tofrom.value, details.value, amount.valueAsNumber];
+
   let doc: HasFormatter;
   if (type.value === 'invoice') {
-    doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    // and then just spread that values instead of typing them each time manually
+    doc = new Invoice(...values);
   } else {
-    doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    // same here
+    doc = new Payment(...values);
   }
 
   list.render(doc, type.value, 'end');
 });
 
-// ENUMS
-// enums are something that can be called a book index - to avoid remembering (thus avoid making mistakew with bad memory) various types of resource type - let's say we want to create an item list, where each item type has different number associated with - 1 for book, 2 for movie, 3 for game, etc. it is easy to forget what number is associated with what category, and it is even harder when we add new ones or modify already exisdting order. That's where enums are handy
-// we just create a list of resource types:
-enum ResourceType { BOOK, AUTHOR, FILM, DIRECTOR };
+// tuples
 
-// and we use that as 'resourceType' property type:
-interface Resource<T> {
-  uid: number;
-  resourceType: ResourceType;
-  data: T;
-}
+// we know, that if we define an array like this
+let arr = ['ryu', 25, true];
+// we can set any of it's element to any type:
+arr[0] = false; 
+arr[1] = 'yoshi';
+//or we can even rewrite the whole array from the start:
+arr = [30, false, 'yoshi'];
+// so any type used in array declaration can take any place in that array
 
-// thanks to that, we now have a tooltip list of all types from 'ResourceType' when we need that
-const docOne: Resource<object> = {
-  uid: 1,
-  resourceType: ResourceType.BOOK,
-  data: { title: 'name of the wind' }
-}
-const docTwo: Resource<object> = {
-  uid: 10,
-  resourceType: ResourceType.DIRECTOR,
-  data: { title: 'name of the wind' }
-}
+// so what if we want to make sure, that specific indexes has specific types? that's a place to use tuples:
+// let tup: [string, number, boolean] = [40, true, 'true'] // expected output: error - we not only have to use the same types as declared, but also in the right positions:
+let tup: [string, number, boolean] = ['true', 30, true];
+tup[0] = 'ken';
+tup[1] = 30;
 
-// and if we console log items, there will be no property name, but a number - property's table index number
-console.log(docOne);
-console.log(docTwo);
+// let student: [string, number];
+// student = ['chun-li', 12321];
